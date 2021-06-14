13c888d26a284cc9c02d7f0f44eaf3e69bf3840f
Saimun <saimun4u@gmail.com>
2021-06-14 17:33:35 -0500

    Initial comment UI using jsx made

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = ()=>{
-    return <div>Hi there</div>
+    return (
+    <div className="ui comments container">
+        <div className = "comment">
+            <a href = "/" className = "avatar">
+                <img alt="avatar" src=""></img>    
+            </a>
+            <div className="content">
+                <a href = "/" className = "content">
+                    Sheen
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00pm</span>
+                </div>
+                <div className="text">Glad to meet you guys!</div>    
+            </div>    
+        </div>
+    </div>
+    )
 }
 
 ReactDOM.render(
