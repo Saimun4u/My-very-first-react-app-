dede10da998e557d9253ff361cfcfa7cfd4f8264
Saimun <saimun4u@gmail.com>
2021-06-15 19:32:32 -0500

    Created a reusable component in a seperate file

--- src/index.js
+++ src/index.js
@@ -19,6 +19,34 @@ const App = ()=>{
                 <div className="text">Glad to meet you guys!</div>    
             </div>    
         </div>
+        <div className = "comment">
+            <a href = "/" className = "avatar">
+                <img alt="avatar" src={faker.image.avatar()}></img>    
+            </a>
+            <div className="content">
+                <a href = "/" className = "content">
+                    Sheen
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00pm</span>
+                </div>
+                <div className="text">Glad to meet you guys!</div>    
+            </div>    
+        </div>
+        <div className = "comment">
+            <a href = "/" className = "avatar">
+                <img alt="avatar" src={faker.image.avatar()}></img>    
+            </a>
+            <div className="content">
+                <a href = "/" className = "content">
+                    Sheen
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at 6:00pm</span>
+                </div>
+                <div className="text">Glad to meet you guys!</div>    
+            </div>    
+        </div>
     </div>
     )
 }
