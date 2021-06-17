798403be21dccf65b7aa018dba8f6a4bab147ad4
Saimun <saimun4u@gmail.com>
2021-06-17 13:36:44 -0500

    Added props to the project

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import faker from 'faker'
 
-const CommentDetail = () => {
+const CommentDetail = props => {
+    console.log(props)
     return (
         <div className = "comment">
             <a href = "/" className = "avatar">
                 <img alt="avatar" src={faker.image.avatar()}></img>    
             </a>
             <div className="content">
-                <a href = "/" className = "content">
-                    Sheen
+                <a href = "/" className = "author">
+                    {props.author}
                 </a>
                 <div className="metadata">
                     <span className="date">Today at 6:00pm</span>
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import CommentDetail from './CommentDetail'
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail/>
+            <CommentDetail author="Tom"/>
+            <CommentDetail author="Jerry"/>
+            <CommentDetail author="Jane"/>
         </div>
     )
 }
