3a6bcc3b1e2e9600bfd0ce4be1671910e74930cd
Saimun <saimun4u@gmail.com>
2021-06-18 20:28:19 -0500

    New card component made for approval reject

---
+++ src/ApprovalCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ApprovalCard = () => {
+    return (
+        <div className = "UI card">
+            <div className = "content">How was the football game today?</div>
+            <div className = "extra content">
+                <div className = "ui two buttons">
+                    <div className = "ui basic green button">Accept</div>
+                    <div className = "ui basic green button">Reject</div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import faker from 'faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail user_pic = {faker.image.avatar()} author="Tom" timeAgo = "Today at 9:00am" comment1="Glad to meet you guys!"/>
-            <CommentDetail user_pic = {faker.image.avatar()} author="Jerry" timeAgo = "Tomorrow at 6:00am" comment1="Hello there"/>
-            <CommentDetail user_pic = {faker.image.avatar()} author="Jane" timeAgo = "Yesterday at 8:30pm" comment1="Hola! Yo soy Jane!"/>
+            <CommentDetail 
+            user_pic = {faker.image.avatar()} 
+            author="Tom" 
+            timeAgo = "Today at 9:00am" 
+            comment1="Glad to meet you guys!"/>
+            <CommentDetail 
+            user_pic = {faker.image.avatar()} 
+            author="Jerry" 
+            timeAgo = "Tomorrow at 6:00am" 
+            comment1="Hello there"/>
+            <CommentDetail 
+            user_pic = {faker.image.avatar()} 
+            author="Jane" 
+            timeAgo = "Yesterday at 8:30pm" 
+            comment1="Hola! Yo soy Jane!"/>
         </div>
     )
 }
