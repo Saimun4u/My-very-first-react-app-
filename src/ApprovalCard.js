7991ba7e46a8ef6b655e3abf91493b0f6e4c292d
Saimun <saimun4u@gmail.com>
2021-06-21 00:42:28 -0500

    Integrated the comments of user blog to approval cards

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-const ApprovalCard = () => {
+const ApprovalCard = props => {
+    console.log(props);
     return (
-        <div className = "UI card">
-            <div className = "content">How was the football game today?</div>
+        <div className = "ui card">
+            <div className = "content">{props.children}</div>
             <div className = "extra content">
                 <div className = "ui two buttons">
                     <div className = "ui basic green button">Accept</div>
-                    <div className = "ui basic green button">Reject</div>
+                    <div className = "ui basic red button">Reject</div>
                 </div>
             </div>
         </div>
     )
 }
+
+export default ApprovalCard;
--- src/index.js
+++ src/index.js
@@ -7,21 +7,27 @@ import ApprovalCard from './ApprovalCard'
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail 
-            user_pic = {faker.image.avatar()} 
-            author="Tom" 
-            timeAgo = "Today at 9:00am" 
-            comment1="Glad to meet you guys!"/>
-            <CommentDetail 
-            user_pic = {faker.image.avatar()} 
-            author="Jerry" 
-            timeAgo = "Tomorrow at 6:00am" 
-            comment1="Hello there"/>
-            <CommentDetail 
-            user_pic = {faker.image.avatar()} 
-            author="Jane" 
-            timeAgo = "Yesterday at 8:30pm" 
-            comment1="Hola! Yo soy Jane!"/>
+            <ApprovalCard>
+                <CommentDetail 
+                    user_pic = {faker.image.avatar()} 
+                    author="Tom" 
+                    timeAgo = "Today at 9:00am" 
+                    comment1="Glad to meet you guys!"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    user_pic = {faker.image.avatar()} 
+                    author="Jerry" 
+                    timeAgo = "Tomorrow at 6:00am" 
+                    comment1="Hello there"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    user_pic = {faker.image.avatar()} 
+                    author="Jane" 
+                    timeAgo = "Yesterday at 8:30pm" 
+                    comment1="Hola! Yo soy Jane!"/>
+            </ApprovalCard>
         </div>
     )
 }
